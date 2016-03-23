9ee83b86c0e96c231c2234b050a9d527a67e7d0a
Julien Berard <jujugrrr@gmail.com>
2016-03-23 14:42:46 +0000

    Fallback to localhost when not instances are returned

--- libraries/get_backends.rb
+++ libraries/get_backends.rb
@@ -1,10 +1,19 @@
 
+# rubocop:disable Metrics/MethodLength
 def get_backends(tag)
   require 'aws-sdk'
   begin
     ec2 = Aws::EC2::Resource.new(region: 'eu-west-1')
-    ec2.instances(filters: [{ name: 'tag:app', values: [tag] }, { name: 'instance-state-name', values: ['running'] }]).map(&:private_ip_address)
+    backends = ec2.instances(filters: [{ name: 'tag:app', values: [tag] }, { name: 'instance-state-name', values: ['running'] }]).map(&:private_ip_address)
+    if backends.empty?
+      Chef::Log.warn('No backend found')
+      ['127.0.0.1']
+    else
+      backends
+    end
   rescue
+    Chef::Log.warn('Cannot retrieve instances from EC2')
     ['127.0.0.1']
   end
 end
+# rubocop:enable Metrics/MethodLength
